cd2939c48255bc03992e8ff366452c44d21a1c28
moon05 <amamun@u.rochester.edu>
2016-05-13 17:21:00 -0400

    changed settings.py

--- App/settings.py
+++ App/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'PomApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates/',],
+        'DIRS': ['/PomApp/templates/',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,5 +121,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR+'/App', 'static')
 STATIC_URL = '/static/'
